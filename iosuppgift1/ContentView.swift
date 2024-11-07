054785b4f406d6b7800d610320bbcc77d6770511
Johan Asklund <jojeask@gmail.com>
2024-11-07 12:34:00 +0100

    Uppgift - Layout

--- iosuppgift1/ContentView.swift
+++ iosuppgift1/ContentView.swift
@@ -8,15 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            VStack(spacing: 0.0) {
+                
+                HStack(spacing: 0.0) {
+                    
+                    VStack {
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .background(Color.red)
+                    
+                    VStack {
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .background(Color.green)
+                    
+                    VStack {
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .background(Color.yellow)
+                }
+                
+                
+                VStack {
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150)
+                .background(Color.gray)
+                
+                
+                VStack {
+                    
+                }
+                .frame(width: 100, height: 100)
+                .background(Color.white)
+                Spacer()
+                
+                
+                HStack(spacing: 0.0) {
+                    ZStack {
+                        
+                    }
+                    .frame(width: 50, height: 60)
+                    .background(Color.black)
+    
+                    
+                    
+                }
+                .padding(.leading, 260)
+                .frame(maxWidth: .infinity, maxHeight: 100)
+                .background(Color.red)
+                
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.cyan)
+            
+            
+            
         }
-        .padding()
+        
     }
+    
 }
 
 #Preview {
