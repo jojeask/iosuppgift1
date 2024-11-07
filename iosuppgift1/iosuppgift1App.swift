4ddbdb7d9b19ee5247d2a6430734b3f520a64753
Johan Asklund <jojeask@gmail.com>
2024-11-07 09:08:43 +0100

    Initial Commit

---
+++ iosuppgift1/iosuppgift1App.swift
@@ -0,0 +1,17 @@
+//
+//  iosuppgift1App.swift
+//  iosuppgift1
+//
+//  Created by Johan Asklund on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct iosuppgift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosuppgift1Tests/iosuppgift1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iosuppgift1Tests.swift
+//  iosuppgift1Tests
+//
+//  Created by Johan Asklund on 2024-11-07.
+//
+
+import Testing
+@testable import iosuppgift1
+
+struct iosuppgift1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
